41634dcc7a7551eeeecac21918b5da292cad87bb
Sebastian Chaparro <50222699+JSebastianChaparro@users.noreply.github.com>
2023-07-06 09:58:55 -0500

    primer commit

---
+++ src/main/java/com/scotiabankcolpatria/hiring/CreditRiskAssessment.java
@@ -0,0 +1,20 @@
+package com.scotiabankcolpatria.hiring;
+
+public class CreditRiskAssessment {
+
+    public double standardDeviation(int[] paymentDelays) {
+        //TODO implement.
+    	System.out.println("primera prueba");
+        return -1;
+    }
+
+    public int paymentDelayMaxPeakIndex(int[] paymentDelays) {
+        //TODO implement.
+        return -1;
+    }
+
+    public double[] latePaymentProbabilityByPeriod(int[][] paymentDelays) {
+        //TODO implement.
+        return null;
+    }
+}
